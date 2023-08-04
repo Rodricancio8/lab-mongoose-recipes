d06a011e42ca929f9cb423f5ad5ada1962e3fe1f
Rodricancio8 <rodri.cancio@hotmail.com>
2023-08-05 00:36:54 +0200

    LAB

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://RodriC8:asdasdd1@rodric8.u1gia4i.mongodb.net/MyFirstDataBase';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +15,54 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+  .then(async() => {
+    Recipe.create({
+      title: 'Milanesa',
+      level: 'Easy Peasy',
+      ingredients: ['breadcrumbs', 'eggs', 'garlic', 'parsley', 'meat'],
+      dishType: 'main_course',
+      duration: 30,
+      creator: 'Lionel Messi',
+
+    })
+
+      .then((newRecipe) => {
+        console.log('New recipe added to the database:', newRecipe.title);
+      })
+
+      .catch(error => {
+        console.error('Error creating your milanesa', error);
+      })
+      
+      Recipe.insertMany(data)
+      
+      .then((dataRecipes) => {
+        console.log('Titles of each recipe:');
+        dataRecipes.forEach((recipe) => {
+          console.log(recipe.title);
+        });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+
+      
+      try {
+        await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+        console.log("Recipe updated");
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    
+    })
+
+
+      
+
+
